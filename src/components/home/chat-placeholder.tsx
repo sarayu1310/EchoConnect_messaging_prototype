a87afd9979b9b7574b9d04cde6e3a8ce803e4a6d
Sarayu <sarayugosika2004@gmail.com>
2024-09-05 20:42:10 +0530

    Ui changes

--- src/components/home/chat-placeholder.tsx
+++ src/components/home/chat-placeholder.tsx
@@ -5,16 +5,13 @@ import { Button } from "../ui/button";
 const ChatPlaceHolder = () => {
 	return (
 		<div className='w-3/4 bg-gray-secondary flex flex-col items-center justify-center py-10'>
-			<div className='flex flex-col items-center w-full justify-center py-10 gap-4'>
-				<Image src={"/desktop-hero.png"} alt='Hero' width={320} height={188} />
-				<p className='text-3xl font-extralight mt-5 mb-2'>Download EchoConnect for Windows</p>
+			<div className='flex flex-col items-center w-full justify-center py-10 gap-2'>
+				<Image src={"/desktop-cover.png"} alt='Hero' width={400} height={280} />
+				<p className='text-3xl font-extralight mb-2'>Echo your thoughts with EchoConnect!</p>
 				<p className='w-1/2 text-center text-gray-primary text-sm text-muted-foreground'>
-					Make calls, share your screen and get a faster experience when you download the Windows app.
+					Make calls, share your screen and get a faster experience.
 				</p>
 
-				<Button className='rounded-full my-5 bg-green-primary hover:bg-green-secondary'>
-					Get from Microsoft Store
-				</Button>
 			</div>
 			<p className='w-1/2 mt-auto text-center text-gray-primary text-xs text-muted-foreground flex items-center justify-center gap-1'>
 				<Lock size={10} /> Your personal messages are end-to-end encrypted
--- src/components/home/theme-switch.tsx
+++ src/components/home/theme-switch.tsx
@@ -21,9 +21,10 @@ const ThemeSwitch = () => {
 					<span className='sr-only'>Toggle theme</span>
 				</Button>
 			</DropdownMenuTrigger>
-			<DropdownMenuContent align='end' className='bg-gray-primary'>
+			<DropdownMenuContent align='end' className='cursor-pointer bg-gray-primary '>
 				<DropdownMenuItem onClick={() => setTheme("light")}>Light</DropdownMenuItem>
 				<DropdownMenuItem onClick={() => setTheme("dark")}>Dark</DropdownMenuItem>
+				<DropdownMenuItem onClick={() => setTheme("system")}>System</DropdownMenuItem>
 			</DropdownMenuContent>
 		</DropdownMenu>
 	);
